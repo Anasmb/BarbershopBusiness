619b42ac1674258c3a6a54e0f2878aa8de9b7f58
Anasmb <40839455+Anasmb@users.noreply.github.com>
2022-03-03 18:32:23 +0300

    + Google Map functionality and API

--- app/src/main/java/com/example/barberbusiness/AddBarbershopActivity.java
+++ app/src/main/java/com/example/barberbusiness/AddBarbershopActivity.java
@@ -3,25 +3,29 @@ package com.example.barberbusiness;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class AddBarbershopActivity extends AppCompatActivity{
 
     private ImageView backBtn;
     private TextInputEditText shopName, email, password, phoneNumber, address;
     private MaterialButton addButton;
+    private String latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,11 +50,25 @@ public class AddBarbershopActivity extends AppCompatActivity{
     private View.OnClickListener addressClickListener = new View.OnClickListener() { //open google map to select location
         @Override
         public void onClick(View view) {
-
+            Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+            startActivityForResult(intent,1);
         }
     };
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) { // get data from google map activity
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == 1){
+            String result = data.getStringExtra("address");
+            String coordinates[] = result.split("/");
+            address.setText(coordinates[0]);
+            latitude = coordinates[1];
+            longitude = coordinates[2];
+            Log.d("debug", "onActivityResult: latitude = " + latitude + "  longitude = " + longitude);
+
+        }
+    }
 
     private View.OnClickListener backBtnListener = new View.OnClickListener() {
         @Override
@@ -62,7 +80,51 @@ public class AddBarbershopActivity extends AppCompatActivity{
     private View.OnClickListener addClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-
+            if(shopName.length() > 0 && phoneNumber.length() > 0 && email.length() > 5 && address.length() > 0 && password.length() > 7 ) {
+                //Start ProgressBar first (Set visibility VISIBLE)
+                addButton.setClickable(false);
+                Handler handler = new Handler(Looper.getMainLooper());
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Starting Write and Read data with URL
+                        //Creating array for parameters
+                        String[] field = new String[2];
+                        field[0] = "ShopName";
+                        field[1] = "PhoneNumber";
+                        field[2] = "Email";
+                        field[3] = "Address";
+                        field[4] = "Status";
+                        //Creating array for data
+                        String[] data = new String[2];
+                        data[0] = String.valueOf(shopName.getText());
+                        data[1] = String.valueOf(phoneNumber.getText());
+                        data[2] = String.valueOf(email.getText());
+                        data[3]= String.valueOf(address.getText());
+                        data[4]= "hidden";
+                        Log.d("php" , data[0] + " " + data[1]);
+                        PutData putData = new PutData("http://192.168.100.6/barbershop-php/registerBarbershop.php", "POST", field, data);
+                        if (putData.startPut()) {
+                            if (putData.onComplete()) {
+                                String result = putData.getResult();
+                                if(result.equals("Login Success")){
+                                    Log.d("php", result);
+                                    Toast.makeText(getApplicationContext(),"Login Success",Toast.LENGTH_LONG).show();
+                                    finish();
+                                    Intent intent = new Intent(getApplicationContext(), AdminMainActivity.class);
+                                    startActivity(intent);
+                                }
+                                else { // All fields are required
+                                    Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
+                                    Log.d("php", result);
+                                    addButton.setClickable(true);
+                                }
+                            }
+                        }
+                        //End Write and Read data with URL
+                    }
+                });
+            }
         }
     };
 
---
+++ app/src/main/java/com/example/barberbusiness/MapsActivity.java
@@ -0,0 +1,103 @@
+package com.example.barberbusiness;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Point;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.barberbusiness.databinding.ActivityMapsBinding;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    private TextView saveLocationText;
+    private double latitude, longitude; // store the coordinates
+    private Geocoder geocoder;
+    List<Address> address; // store the address from the google map
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        window.setStatusBarColor(this.getResources().getColor(R.color.design_default_color_secondary_variant));
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        saveLocationText = findViewById(R.id.saveLocationText);
+        saveLocationText.setOnClickListener(saveLocationClick);
+        geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    private View.OnClickListener saveLocationClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent();
+            intent.putExtra("address",  address.get(0).getAddressLine(0) + "/" + latitude + "/" + longitude);
+            setResult(Activity.RESULT_OK, intent);
+            finish();
+        }
+    };
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng riyadh = new LatLng(24.726, 46.710);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(riyadh));
+        mMap.setMinZoomPreference(11);
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(latLng));
+                latitude = latLng.latitude;
+                longitude = latLng.longitude;
+                try {
+                     address = geocoder.getFromLocation(latitude,longitude,1);
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+
+                Log.d("googlemap", "onMapClick: latitude = " + latitude + " , longitude = " + longitude);
+                Log.d("googlemap", "Address = " + address.get(0).getAddressLine(0));
+            }
+        });
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                marker.remove();
+                return true;
+            }
+        });
+    }
+
+
+}
